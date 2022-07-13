1a1faffafb27e4821e9b7245798458b1453d97d9
Rishi Balamurugan <rishi.b2372@gmail.com>
2022-07-13 13:12:17 +0530

    feature: crop, backend result (number, image& JSON)

--- src/App.js
+++ src/App.js
@@ -1,66 +1,75 @@
 import React, { useEffect, useState } from 'react';
-import ReactCrop, {
-  Crop,
-  PixelCrop,
-} from 'react-image-crop'
+import ReactCrop from 'react-image-crop'
 
 import 'react-image-crop/dist/ReactCrop.css'
 import './App.css';
 
 function App() {
 	const [file, setFile] = useState(null);
+	const [prediction, setPrediction] = useState(null);
 	const [result, setResult] = useState("");
 	const [resultImage, setResultImage] = useState(null);
   const [crop, setCrop] = useState(null);
 	const [completedCrop, setCompletedCrop] = useState(null);
 	
-	// async function loadModel() {
-	// 	// const model = await tf.loadLayersModel("best_web_model/model.json");
-	// 	const model = await tf.loadGraphModel(process.env.PUBLIC_URL + "/best_web_model/model.json");
-	// 	setModel(model)
-	// }
-	
 	async function predict() {
-		// const image = new Image();
-		// image.src = file;
-		// image.onload = async () => {
-		// 	const tensor = tf.browser.fromPixels(image);
-		// 	const tensor2 = tensor.resizeNearestNeighbor([640, 640]).toFloat();
-		// 	const prediction = await model.executeAsync(tensor2.expandDims(), ['detection_classes', 'detection_scores']);
-		// 	console.log(prediction[0].dataSync());
-		// }
-		// setResult(result.toString());
-		
-		// send file to api
 		const formData = new FormData();
-		formData.append(
-			"file",
-			file,
-			file.name
-		);
-		const response = await fetch("http://localhost:8000/object-to-json", {
+		formData.append("file", file, file.name);
+		formData.append("crop", JSON.stringify(completedCrop));
+		fetch("http://localhost:8000/object-to-json", {
 			method: "POST",
 			body: formData,
-		});
+		}).then( async (response) => {
+			const result = await response.json();
+			setResult(JSON.stringify(result, null, 2));	
+			
+	});
 		
-		const responseImage = await fetch("http://localhost:8000/object-to-img", {
+		fetch("http://localhost:8000/object-to-img", {
 			method: "POST",
 			body: formData,
+		}).then( async (response) => {
+			const resultImage = await response.blob();
+			setResultImage(URL.createObjectURL(resultImage));
 		});
 		
-		const result = await response.json();
-		setResult(JSON.stringify(result, null, 2));
 		
-		const resultImage = await responseImage.blob();
-		setResultImage(URL.createObjectURL(resultImage));
 	}
 	
-	// useEffect(() => {
-	// 	loadModel();
-	// }, []);
+	useEffect(() => {
+		if (result) {
+			const resultDict = JSON.parse(result)["result"];
+			
+			const sortedResultDict = resultDict.sort((a, b) => {
+				return a.xmin - b.xmin;
+			});
+			
+			const digits = sortedResultDict.map((item) => {
+				return item.name;
+			});
+			setPrediction(digits);
+		}
+	}, [result]);
 	
   return (
-    <div className="App">
+		<div className="App">
+			<input
+				type="button"
+				value="Test"
+				style={{
+					padding: '20px',
+					margin: '20px',
+				}}
+				onClick={ async () => {
+					const form = new FormData();
+					form.append("file", file, file.name);
+					form.append("crop", JSON.stringify(completedCrop));
+					const response = await fetch("http://localhost:8000/test", {
+						method: "POST",
+						body: form,
+					});
+					console.log(response);
+				}} />
 			<input type="file" onChange={(event) => {
 				setFile(event.target.files[0])
 				setResult("");
@@ -84,6 +93,7 @@ function App() {
 					</ReactCrop>
 					<p> Crop: { JSON.stringify(completedCrop) }</p>
 					<button type="button" onClick={predict}>Predict</button>
+					<p>{prediction}</p>
 					{resultImage && 
 						<div>
 							<img
@@ -92,9 +102,9 @@ function App() {
 								width="400"
 								height="400"
 							/>
-							<pre>{result}</pre>
 						</div>
 					}
+					<pre>{result}</pre>
 				</div>
 			}
     </div>
